7abe795a7beac3acd9b515340a4d603235078b1c
Ahnaf Taha <taha1120@gmail.com>
2018-03-19 03:58:25 -0400

    First Update
    (Desktop only); Developed basic front page layout

---
+++ js/ahnaf.js
@@ -0,0 +1,17 @@
+$(".about").click(function(){
+    $("#intro").transition({ y: '-100vh', duration: 1000 });
+    $("#about").transition({ y: '0', duration: 1000 });
+    $("#info-box").transition({ y: '100vh', duration: 1000 });
+});
+
+$(".work").click(function(){
+    $("#intro").transition({ x: '100vw', duration: 1000 });
+    $("#work").transition({ x: '0', duration: 1000 });
+    $("#info-box").transition({ x: '-100vw', duration: 1000 });
+});
+
+$(".contact").click(function(){
+    $("#intro").transition({ x: '-100vw', duration: 1000 });
+    $("#contact").transition({ x: '0', duration: 1000 });
+    $("#info-box").transition({ x: '100vw', duration: 1000 });
+});
