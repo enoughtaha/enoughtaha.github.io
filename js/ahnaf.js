acb502e21d5163ee0bbf1903fa951d8d94979d8f
Ahnaf Taha <taha1120@gmail.com>
2018-03-19 17:01:24 -0400

    New buttons
    Navigation buttons

--- js/ahnaf.js
+++ js/ahnaf.js
@@ -7,15 +7,27 @@ $(".about").click(function(){
     $("#about").transition({ y: '0', duration: 1000 });
     $("#info-box").transition({ y: '100vh', duration: 1000 });
 });
+    $("#about-ahnaf").click(function(){
+        $("#intro").transition({ y: '0', duration: 1000 });
+        $("#info-box").transition({ y: '0', duration: 1000 });
+    });
 
 $(".work").click(function(){
     $("#intro").transition({ x: '100vw', duration: 1000 });
     $("#work").transition({ x: '0', duration: 1000 });
     $("#info-box").transition({ x: '-100vw', duration: 1000 });
 });
+    $("#work-ahnaf").click(function(){
+        $("#intro").transition({ x: '0', duration: 1000 });
+        $("#info-box").transition({ x: '0', duration: 1000 });
+    });
 
 $(".contact").click(function(){
     $("#intro").transition({ x: '-100vw', duration: 1000 });
     $("#contact").transition({ x: '0', duration: 1000 });
     $("#info-box").transition({ x: '100vw', duration: 1000 });
 });
+    $("#contact-ahnaf").click(function(){
+        $("#intro").transition({ x: '0', duration: 1000 });
+        $("#info-box").transition({ x: '0', duration: 1000 });
+    });
